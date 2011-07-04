4e67541a92f9ae4c7417d3b8e4e0fe4dbac8befb
Abraham Flaxman <abie@alum.mit.edu>
2011-07-04 15:02:13 -0700

    picture perfect data viz

--- src/graphics.py
+++ src/graphics.py
@@ -7,8 +7,10 @@ def plot_all_data():
     import data
     pl.clf()
     pl.plot(data.hdi, data.tfr, 'ko', ms=6, mew=0)
-    pl.plot(data.all.hdi1975, data.all.tfr1975, 'bs', ms=10, mew=0, label='1975')
-    pl.plot(data.all.hdi2005, data.all.tfr2005, 'r^', ms=12, mew=0, label='2005')
+    pl.plot(data.all.hdi1975, data.all.tfr1975, 'bs',
+            ms=10, mew=0, label='1975')
+    pl.plot(data.all.hdi2005, data.all.tfr2005, 'r^',
+            ms=12, mew=0, label='2005')
     
     pl.axis([.3, .99, 1., 8.])
     pl.legend()
