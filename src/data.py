1e76427a2c1b010c08bcb0408dfeefcd46d82016
Abraham Flaxman <abie@alum.mit.edu>
2011-07-04 15:00:00 -0700

    visualize data

--- src/data.py
+++ src/data.py
@@ -1,10 +1,10 @@
 import pylab as pl
 
-all_data = pl.csv2rec('nature08230-s2.csv')
+all = pl.csv2rec('nature08230-s2.csv')
 
 years = range(1975, 2006)
-hdi = [d['hdi%d'%y] for y in years for d in all_data]
+hdi = [d['hdi%d'%y] for y in years for d in all]
 hdi = pl.ma.masked_array(hdi, pl.isnan(hdi))
 
-tfr = [d['tfr%d'%y] for y in years for d in all_data]
+tfr = [d['tfr%d'%y] for y in years for d in all]
 tfr = pl.ma.masked_array(tfr, pl.isnan(tfr))
--- src/graphics.py
+++ src/graphics.py
@@ -3,3 +3,14 @@ import pylab as pl
 
 # put messy matplotlib code here so I don't have to look at it if I
 # don't want to
+def plot_all_data():
+    import data
+    pl.clf()
+    pl.plot(data.hdi, data.tfr, 'ko', ms=6, mew=0)
+    pl.plot(data.all.hdi1975, data.all.tfr1975, 'bs', ms=10, mew=0, label='1975')
+    pl.plot(data.all.hdi2005, data.all.tfr2005, 'r^', ms=12, mew=0, label='2005')
+    
+    pl.axis([.3, .99, 1., 8.])
+    pl.legend()
+    pl.xlabel('Human development index', fontsize=18)
+    pl.ylabel('Total fertility rate', fontsize=18)
